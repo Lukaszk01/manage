a683b27330c7b04001cca61c18fee74226493440
Lukaszk01 <lukaszkkoziel@gmail.com>
2021-09-22 09:41:42 +0900

    personal informations style

--- src/views/StaffPage.vue
+++ src/views/StaffPage.vue
@@ -1,24 +1,50 @@
 <template>
-  <div class="staffPage">
-    <h1>This is an Staff page</h1>
+   <div id="app" class="d-flex justify-content-end">
+    <h1>Date: </h1>
+    <p>{{currentDate()}}</p>
   </div>
-
-    <div id="nav" class="w-25 p-3">
+<div class="d-flex justify-content-start">
+  <div id="nav" class="w-25">
     <ul class="list-group list-group-flush">
-      <li class="list-group-item d-flex justify-content-between align-items-center">
+      <li class="list-group-item d-flex justify-content-startd-flex justify-content-between">
         <router-link to="/personalDetail">Personal Details</router-link></li>
-      <li class="list-group-item d-flex justify-content-between align-items-center">
+      <li class="list-group-item d-flex justify-content-startd-flex justify-content-between">
         <router-link to="/shifts">My shifts</router-link></li>
-      <li class="list-group-item d-flex justify-content-between align-items-center">
+      <li class="list-group-item d-flex justify-content-startd-flex justify-content-between">
         <router-link to="/">Notifications</router-link>
           <span class="badge bg-primary rounded-pill">4</span>
       </li>
-      <li class="list-group-item d-flex justify-content-between align-items-center">
+      <li class="list-group-item d-flex justify-content-startd-flex justify-content-between">
         <router-link to="/">Availability</router-link></li>
-      <li class="list-group-item d-flex justify-content-between align-items-center">
+      <li class="list-group-item d-flex justify-content-startd-flex justify-content-between">
         <router-link to="/">Chat</router-link></li>
   </ul>
   </div>
   <router-view/>
+</div>
+    
 </template>
 
+<script>
+export default {
+  methods: {
+    currentDate() {
+      const current = new Date();
+      const date = `${current.getDate()}/${current.getMonth()+1}/${current.getFullYear()}`;
+      return date;
+    }
+  }
+};
+
+</script>
+
+<style scoped>
+#app {
+  padding: 20px;
+
+}
+#app h1 {
+  font-size: 22px;
+  margin-right: 5px;
+}
+</style>
