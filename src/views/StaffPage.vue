9035a4d2df888b52f0288d2439597d66eaf9bbb8
Lukaszk01 <lukaszkkoziel@gmail.com>
2021-10-09 08:48:46 +0900

    button fade

--- src/views/StaffPage.vue
+++ src/views/StaffPage.vue
@@ -24,17 +24,48 @@
   </ul>
   </div>
   <router-view/>
+     <div>
+    <button @click="toggleModal">Open</button>
+
+    <transition name="fade">
+      <div v-if="isOpen" class="modal">
+        <p>
+          <button @click="toggleModal">Close</button>
+        </p>
+      </div>
+    </transition>
+  </div>
 </div>
     
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      isOpen: false,
+      query: this.getInitialQueryValue()
+    };
+  },
+  watch: {
+    $route() {
+      this.query = this.getInitialQueryValue()
+    }
+  },
   methods: {
     currentDate() {
       const current = new Date();
       const date = `${current.getDate()}/${current.getMonth()+1}/${current.getFullYear()}`;
       return date;
+    },
+    getInitialQueryValue() {
+      return this.$route.query.search || "";
+    },
+    emitQuery() {
+      this.$emit("submit:query", this.query);
+    },
+    toggleModal() {
+      this.isOpen = !this.isOpen
     }
   }
 };
@@ -58,5 +89,24 @@ export default {
   padding: 50px;
   margin-left: 50px;
   margin-bottom: 20px;
+}
+.fade-enter {
+    opacity: 0;
+  }
+  .fade-enter-active,
+  .fade-leave-active {
+    transition: opacity 0.5s ease-out;
+  }
+  .fade-leave-to {
+    opacity: 0;
+  }
+  .modal {
+    width: 250px;
+    height: 200px;
+    padding: 10px;
+    margin: 0 auto;
+    margin-top: 20px;
+    border-radius: 2%;
+    background-color: #e0e0e0;
 }
 </style>
