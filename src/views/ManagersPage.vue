20d0141063bf215bdcc12bb8bcc65bc98d675e8e
Lukaszk01 <lukaszkkoziel@gmail.com>
2021-10-16 23:12:51 +0900

    managers page

--- src/views/ManagersPage.vue
+++ src/views/ManagersPage.vue
@@ -1,85 +1,37 @@
 <template>
-  <div>
-    <div class="flex-table">
-      <div>Name</div>
-      <div># videos</div>
-      <div></div>
-    </div>
-    <div v-for="tag in tags" :key="tag.id" class="flex-table">
-      <div>
-        <div v-if="tagEditingId == tag.id">
-          <v-text-field v-model="tag.name"
-                        :id="`tag-edit-${tag.id}`"
-                        @blur="updateTagName(tag)"
-                        @keydown.enter="updateTagName(tag)" />
-        </div>
-        <div v-else @click="setToEditing(tag)">
-          {{tag.name}}
-        </div>
-      </div>
-      <div>
-        <router-link :to="{ name: 'tag', params: { id: tag.id }}">
-          {{tag.video_ids.length}}
-        </router-link>
-      </div>
-      <div class="actions">
-        <v-btn x-small @click="setToEditing(tag)">Edit</v-btn>
-        <v-btn x-small @click="deleteTag(tag)">Delete</v-btn>
-      </div>
-    </div>
-    <div v-if="!isEditingNewTag">
-      <v-btn @click="startNewTag()">Add Tag</v-btn>
+<input v-model="name" placeholder="edit me">
+<p>Name is: {{ name }}</p>
+
+ <div v-if="!isEditingName">
+      <button @click="startName()">Add Tag</button>
     </div>
     <div v-else>
-      <v-text-field v-model="newTagName"
-                    :id="`new-tag-name`"
-                    @blur="createTag()"
-                    @keydown.enter="createTag()" />
+      <v-text-field v-model="Name" :id="`new-tag-name`" @blur="createTag()" @keydown.enter="createTag()" />
     </div>
-  </div>
 </template>
 
 <script>
-import { mapState } from 'vuex';
-  export default {
+    export default {
     data() {
       return {
-        tagEditingId: '',
-        isEditingNewTag: false,
-        newTagName: '',
+        isEditingName: false,
+        Name: '',
       }
     },
     computed: {
-      ...mapState({
-        tags: state => state.tags.tags
-      })
-    },
-    methods: {
-      setToEditing(tag) {
-        this.tagEditingId = tag.id;
-        setTimeout(()=> {
-          document.getElementById(`tag-edit-${tag.id}`).focus()
-        }, 1)
-      },
-      updateTagName(tag) {
-        this.$store.dispatch('tags/updateName', {tag})
-        this.tagEditingId = ''
-      },
-      deleteTag(tag) {
-        let confirmed = confirm(`Are you sure you want to delete tag ${tag.name}? It is connected to ${tag.video_ids.length} videos.`)
         if(confirmed){
-          this.$store.dispatch('tags/delete', {tag});
+          this.$store.dispatch('deleteTag', {tag});
         }
       },
-      startNewTag(){
-        this.isEditingNewTag = true;
+      startName(){
+        this.isEditingName = true;
         setTimeout(()=> {
           document.getElementById(`new-tag-name`).focus()
         }, 1)
       },
       createTag(){
         if(this.newTagName.length > 0) {
-          this.$store.dispatch('tags/create', {name: this.newTagName})
+          this.$store.dispatch('createTag', {name: this.newTagName})
           this.newTagName = ''
         }
         this.isEditingNewTag = false
@@ -89,18 +41,5 @@ import { mapState } from 'vuex';
 </script>
 
 <style scoped>
-  .flex-table {
-    display: grid;
-    grid-template-columns: repeat(auto-fill, 33%);
-    padding: 10px;
-    border-bottom: 1px black solid;
-    &:nth-of-type(2n) {
-      background-color: #dedede;
-    }
-    .actions {
-      * {
-        margin-right: 10px
-      }
-    }
-  }
-</style>
+
+</style> 
