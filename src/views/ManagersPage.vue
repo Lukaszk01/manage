6e46f1247ef4ad47a73386ca34b4ef1ab14d69f7
Lukaszk01 <lukaszkkoziel@gmail.com>
2021-10-18 23:33:38 +0900

    cleaning

--- src/views/ManagersPage.vue
+++ src/views/ManagersPage.vue
@@ -1,41 +1,18 @@
 <template>
 <input v-model="name" placeholder="edit me">
 <p>Name is: {{ name }}</p>
-
- <div v-if="!isEditingName">
-      <button @click="startName()">Add Tag</button>
-    </div>
-    <div v-else>
-      <v-text-field v-model="Name" :id="`new-tag-name`" @blur="createTag()" @keydown.enter="createTag()" />
-    </div>
 </template>
 
 <script>
-    export default {
+  export default {
     data() {
       return {
-        isEditingName: false,
-        Name: '',
-      }
-    },
-    computed: {
-        if(confirmed){
-          this.$store.dispatch('deleteTag', {tag});
-        }
-      },
-      startName(){
-        this.isEditingName = true;
-        setTimeout(()=> {
-          document.getElementById(`new-tag-name`).focus()
-        }, 1)
-      },
-      createTag(){
-        if(this.newTagName.length > 0) {
-          this.$store.dispatch('createTag', {name: this.newTagName})
-          this.newTagName = ''
-        }
-        this.isEditingNewTag = false
-      }
+        name: '',
+        fullName: []
+             }
+          },
+    methods: {
+    
     }
   }
 </script>
