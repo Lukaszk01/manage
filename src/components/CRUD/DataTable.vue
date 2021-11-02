24b722f73bdeb12b16e1de16d81c178a27692f15
Lukaszk01 <lukaszkkoziel@gmail.com>
2021-11-02 16:32:14 +0900

    create customer

--- src/components/CRUD/DataTable.vue
+++ src/components/CRUD/DataTable.vue
@@ -124,6 +124,50 @@
       ></delete-customer-modal>
     </b-modal>
   </div>
+
+  <b-modal
+      ref="create-customer-modal"
+      size="xl"
+      hide-footer
+      title="New Customer"
+    >
+      <create-customer-form
+        @closeCreateModal="closeCreateModal"
+        @reloadDataTable="getCustomerData"
+        @showSuccessAlert="showAlertCreate"
+      ></create-customer-form>
+    </b-modal>
+
+    <!-- Modal for updating customers -->
+    <b-modal
+      ref="edit-customer-modal"
+      size="xl"
+      hide-footer
+      title="Edit Customer"
+    >
+      <edit-customer-form
+        @closeEditModal="closeEditModal"
+        @reloadDataTable="getCustomerData"
+        @showSuccessAlert="showAlertUpdate"
+        :customerId="customerId"
+      ></edit-customer-form>
+    </b-modal>
+
+<!-- Delete Customer Modal -->
+    <b-modal
+      ref="delete-customer-modal"
+      size="md"
+      hide-footer
+      title="Confirm Deletion"
+    >
+      <delete-customer-modal
+        @closeDeleteModal="closeDeleteModal"
+        @reloadDataTable="getCustomerData"
+        @showDeleteAlert="showDeleteSuccessModal"
+        :customerId="customerId"
+      ></delete-customer-modal>
+    </b-modal>
+    
 </template>
 
 <script>
