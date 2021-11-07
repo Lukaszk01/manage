6a20499aa43da7841ac8a7295d1ea29d4684e1bf
Lukaszk01 <lukaszkkoziel@gmail.com>
2021-11-07 12:23:18 +0900

    managing routes and components

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,37 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div>
+    <container>
+      <div class="mt-5">
+        <HeaderBar />
+        <EditCustomerForm /> 
+        <DeleteCustomerModal /> 
+        <DataTable />
+        <CustomerOverview />
+        <CreateCustomerForm />
+        <router-view />
+      </div>
+    </container>
   </div>
 </template>
+
+
+<script>
+import HeaderBar from "@/components/CRUD/HeaderBar.vue";
+import EditCustomerForm from "@/components/CRUD/EditCustomerForm.vue";
+import DeleteCustomerModal from "@/components/CRUD/DeleteCustomerModal.vue";
+import DataTable from "@/components/CRUD/DataTable.vue";
+import CustomerOverview from "@/components/CRUD/CustomerOverview.vue";
+import CreateCustomerForm from "@/components/CRUD/CreateCustomerForm.vue";
+
+
+export default {
+  components: {
+    HeaderBar,
+    EditCustomerForm, 
+    DeleteCustomerModal, 
+    DataTable,
+    CustomerOverview,
+    CreateCustomerForm 
+  },
+};
+</script>
