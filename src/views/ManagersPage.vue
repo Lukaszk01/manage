47d099919bd9e93a63826676539b1e38c95bd99b
Lukaszk01 <lukaszkkoziel@gmail.com>
2021-12-15 21:33:55 +0900

    reverse

---
+++ src/views/ManagersPage.vue
@@ -0,0 +1,30 @@
+<template>
+<div id="list-rendering">
+  <ol>
+    <li v-bind:todo in todos>
+      {{ todo.text }}
+    </li>
+  </ol>
+</div>
+</template>
+
+
+<script>
+export default {
+  const ListRendering = {
+  data() {
+    return {
+      todos: [
+        { text: 'Learn JavaScript' },
+        { text: 'Learn Vue' },
+        { text: 'Build something awesome' }
+      ]
+    }
+  }
+}
+
+
+
+  }
+Vue.createApp(ListRendering).mount('#list-rendering')  
+</script>
