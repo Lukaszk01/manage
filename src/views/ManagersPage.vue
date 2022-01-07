3a3b7d5fed12d8487c2dc17806c6a4e2fbf77624
Lukaszk01 <lukaszkkoziel@gmail.com>
2022-01-07 18:56:55 +0900

    new crud

--- src/views/ManagersPage.vue
+++ src/views/ManagersPage.vue
@@ -1,158 +1,288 @@
 <template>
-  <div id="app">
-  <div class="row p-2">
-    <div class="col-6">
-      <h2>List</h2>
-      <transition-group class="list-group" name="employee-list" tag="ul">
-        <li class="list-group-item employee-list-item" v-for="item in items" :key="item.id">
-          <div class="d-flex align-items-center">
-           <div>
-            {{ item.name }}
-           </div>
-           <div class="ml-auto">
-            <button type="button" @click="removeItem" class="btn btn-danger btn-sm">
-              X
-            </button>
-            <button type="button" @click="addItem" class="btn btn-warning btn-sm">
-              +
-            </button>
-            <button type="button" @click="moveItems" class="btn btn-primary btn-sm ml-3">
-              Move
-            </button>
-           </div>
-          </div>
-        </li>
-      </transition-group>
-      <h3>Number of items: {{ items.length }}</h3>
+  <div id="shopping-list">
+    <div class="header">
+      <h1>{{ header || 'Welcome' }}</h1>
+      <button v-if="editing" @click="doEdit(false)" class="btn btn-cancel">Cancel</button>
+      <button v-else @click="doEdit(true)" class="btn btn-primry">Add Staff</button>
     </div>
-    <div class="col-6">
-      <h2>Table</h2>
-      <table class="table mb-0">
-        <thead>
-          <tr>
-            <th scope="col">Employee</th>
-            <th scope="col">Email</th>
-          </tr>
-        </thead>
-        <tbody name="employee-table">
-          <tr class="employee-table-item" v-for="item in items" :key="item.id">
-            <td scope="row">
-                {{ item.name }}
-            </td>
-            <td>
-                {{ item.quantity }}
-            </td>
-          </tr>
-        </tbody>
-      </table>
-      <h3>Total quantity: {{ totalQuantity }}</h3>
+    
+    <div v-if="editing" class="add-item-form">
+      <input 
+        @keyup.enter="saveDetail" 
+        type="text" v-model="newFirstName" placeholder = "First Name">
+      <input 
+        @keyup.enter="saveDetail" 
+        type="text" v-model="newDetail" placeholder = "Last Name">
+      <input 
+        @keyup.enter="saveDetail" 
+        type="text" v-model="newDetail" placeholder = "Email">
+      <label>
+        <input type="checkbox" v-model="newDetailHighPriority">
+        High Priority
+      </label>
+      <button 
+        @click="saveDetail" 
+        class="btn btn-primary">
+        Save Detail
+      </button>
     </div>
+    <p v-if="items.length === 0">Nice job! You've bought all your items!</p>
+    <ul>
+      <li 
+        v-for="item in reversedDetails" 
+        @click="togglePurchased(item)"
+        :key="item.id"
+        class="static-class"
+        :class="{strikeout: item.purchased, priority: item.highPriority}"
+      >
+        {{item.label}}
+      </li>
+      <li 
+      v-for="item in reversedDetails" 
+      @click="togglePurchased(item)"
+      :key="item.id"
+      class="static-class"
+      :class="{strikeout: item.purchased, priority: item.highPriority}"
+    >
+      {{item.label}}
+    </li>
+    
+    </ul>
   </div>
-</div>
+
+  <table class="table">
+    <thead>
+      <tr>
+        <th scope="col">#</th>
+        <th scope="col">First</th>
+        <th scope="col">Last</th>
+        <th scope="col">Handle</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <th scope="row">1</th>
+        <td>Mark</td>
+        <td>Otto</td>
+        <td>@mdo</td>
+      </tr>
+      <tr>
+        <th scope="row">2</th>
+        <td>Jacob</td>
+        <td>Thornton</td>
+        <td>@fat</td>
+      </tr>
+      <tr>
+        <th scope="row">3</th>
+        <td colspan="2">Larry the Bird</td>
+        <td>@twitter</td>
+      </tr>
+    </tbody>
+  </table>
 </template>
 
 <script>
-
-
-export default ({
-
-  el: "#app",
-  
+export default {
   data() {
-  	return {
-      items: [
-        {
-          id: 1,
-          name: "John Smith",
-          quantity: 5
-        }, {
-          id: 2,
-          name: "Matt Rat",
-          quantity: 3
-        }, {
-          id: 4,
-          name: "Jim Beam",
-          quantity: 1
-        }, {
-          id: 5,
-          name: "Tony Malony",
-          quantity: 25
+        return {
+          header: 'Shopping List App',
+          editing:false,
+          newDetail: '',
+          newDetailHighPriority: false,
+          items:[
+            {id: 1, label:'10 party hats', purchased: true, highPriority: false},
+            {id: 2, label:'2 board games', purchased: true, highPriority: false},
+            {id: 3, label:'20 cups', purchased: false, highPriority: true},
+          ]
+        }
+      },
+      computed:{
+        reversedDetails(){
+          return [...this.items].reverse()
+        }
+      },
+      methods:{
+        saveDetail(){
+          this.items.push({
+            id:this.items.length + 1, 
+            label: this.newDetail,
+            highPriority: this.newDetailHighPriority
+          })
+          this.newDetail = ""
+          this.newDetailHighPriority = false
         },
-      ]
-    }
-  },
-  
-  computed: {
-  	totalQuantity() {
-    	return this.items.reduce((total, item) => {
-      	return total + item.quantity
-      }, 0)
-    },
-  },
-  
-  mounted() {
-  	this.addItem()
-  },
-  
-  methods: {
-  	addItem() {
-    	this.items.splice(2, 0, {
-      	id: 3,
-      	name: "Kiwis",
-      	quantity: 8
-      })
-    },
-    moveItems() {
-      this.items = this.items.reverse()
-      setTimeout(() => {
-      	this.removeItem()
-      }, 2000)
-    },
-    removeItem() {
-      this.items.splice(2, 1)
-      setTimeout(() => {
-      	this.addItem()
-      }, 2000)
-    }
-  }
-})
+        doEdit(editing){
+          this.editing = editing
+          this.newDetail = ""
+          this.newDetailHighPriority = false
+        },
+        togglePurchased(item){
+          item.purchased = !item.purchased
+        }
+      }
+}
 </script>
 
 
 <style>
-.employee-list-item {
-  transition: all 1s;
-}
-.employee-list-enter,
-.employee-list-leave-to {
-  max-height: 0px;
-  padding-top: 0px !important;
-  padding-bottom: 0px !important;
-  overflow: hidden;
-}
-.employee-list-enter-to,
-.employee-list-leave {
-  max-height: 80px;
-}
-
-.employee-table-item {
-  transition: all 1s;
-}
-.employee-table-item > * {
-  transition: all 1s;
-  overflow: hidden;
-}
-.employee-table-enter,
-.employee-table-leave-to {
-  line-height: 0;
-}
-.employee-table-enter > *,
-.employee-table-leave-to > * {
-  padding-top: 0px !important;
-  padding-bottom: 0px !important;
-}
-.employee-table-enter-to,
-.employee-table-leave {
-  line-height: 1.5;
-}
+  body {
+    background: #eff8ff;
+    height: 100vh;
+    width: 100vw;
+    font-family: system-ui, BlinkMacSystemFont, -apple-system, Segoe UI, Roboto,
+      Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin: 0;
+    padding: 0;
+  }
+  
+  .counter {
+    font-size: 0.8rem;
+    padding-left: 10px;
+    padding-right: 10px;
+  }
+  
+  #shopping-list {
+    background: #fff;
+    padding: 2rem;
+    margin: 1rem;
+    border-radius: 3px;
+    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.12), 0 2px 4px 0 rgba(0, 0, 0, 0.08);
+    width: 95%;
+    max-width: 900px;
+  }
+  
+  h1 {
+    color: #3d4852;
+  }
+  
+  ul {
+    list-style: none;
+    padding: 0;
+  }
+  
+  a {
+    color: #6cb2eb;
+    font-size: 1.25rem;
+    transition: all 0.1s ease-in;
+    margin-top: 0.5rem;
+    display: block;
+  }
+  
+  a:hover {
+    color: #3490dc;
+  }
+  
+  li,
+  p {
+    display: flex;
+    align-items: center;
+    line-height: 1.75;
+    letter-spacing: 0.5px;
+    color: #3d4852;
+    font-size: 1.25rem;
+    cursor: pointer;
+    transition: all 0.1s ease-in;
+  }
+  
+  li:hover {
+    color: #22292f;
+  }
+  
+  li input {
+    margin: 0 0.5rem 0;
+  }
+  
+  #shopping-list > input,
+  #shopping-list > select {
+    width: 100%;
+    border-radius: 3px;
+    box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.1);
+    border: 1px solid #f1f5f8;
+    color: #606f7b;
+    padding: 0.5rem 0.75rem;
+    box-sizing: border-box;
+    font-size: 1rem;
+    letter-spacing: 0.5px;
+    margin: 0.5rem 0;
+  }
+  
+  .add-item-form,
+  .header {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+  
+  .add-item-form input {
+    width: 70%;
+    border-radius: 3px;
+    box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.1);
+    border: 1px solid #f1f5f8;
+    color: #606f7b;
+    padding: 0.5rem 0.75rem;
+    box-sizing: border-box;
+    font-size: 1rem;
+    letter-spacing: 0.5px;
+    margin: 0.5rem 0;
+  }
+  
+  .btn {
+    border: none;
+    border-radius: 3px;
+    margin: auto 0;
+    padding: 0.5rem 0.75rem;
+    flex-shrink: 0;
+    cursor: pointer;
+    font-size: 0.9rem;
+    letter-spacing: 0.5px;
+    transition: all 0.1s ease-in;
+  }
+  
+  .btn[disabled] {
+    background: #8795a1;
+  }
+  
+  .btn[disabled]:hover {
+    background: #606f7b;
+  }
+  
+  .btn-primary {
+    background: #6cb2eb;
+    color: #fff;
+  }
+  
+  .btn-primary:hover {
+    background: #3490dc;
+  }
+  
+  .btn-cancel {
+    background: #ef5753;
+    color: #fff;
+  }
+  
+  .btn-cancel:hover {
+    background: #e3342f;
+    color: #fff;
+  }
+  
+  .strikeout {
+    text-decoration: line-through;
+    color: #b8c2cc;
+  }
+  
+  .strikeout:hover {
+    color: #8795a1;
+  }
+  
+  .priority {
+    color: #de751f;
+  }
+  input[type="checkbox"]{
+    display: inline !important;
+    box-shadow: none;
+    width: auto;
+  }
 </style>
